4471e3305b0af45c46c6a3e2fdbfb4963ba0a105
Matthew Richardson <M.Richardson@ed.ac.uk>
2020-05-12 14:13:42 +0100

    Fix deps and imports.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+// FIXME: Make theme a customisable option
+import 'primevue/resources/themes/nova-light/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
 export * from './install'
