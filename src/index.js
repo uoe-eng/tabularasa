a93fd9309a982aad98759ec7c9dff38bf1b09c0e
Matthew Richardson <M.Richardson@ed.ac.uk>
2021-01-26 12:45:59 +0000

    Update for Vue3 in 'root'.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // FIXME: Make theme a customisable option
-import 'primevue/resources/themes/nova-light/theme.css'
+import 'primevue/resources/themes/nova-vue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
