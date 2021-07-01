93df0974e83af6afdf83a75adc9f7babcc5a3773
Matthew Richardson <M.Richardson@ed.ac.uk>
2021-07-01 09:15:26 +0100

    Only disable consold/debugging in dev.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     'prettier-vue/prettier': 'warn',
     camelcase: ["error", {"properties": "never"}],
-    'no-console': 'off',
+    'no-console': process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-debugger': process.env.NODE_ENV === "production" ? "error" : "off"
   },
 }
