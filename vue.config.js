ca2d7667c285826eb556153826893da803a02748
Matthew Richardson <M.Richardson@ed.ac.uk>
2021-09-11 19:26:25 +0100

    Create a gh-pages build method in pakcages.json

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 // Set app 'root' to testapp for 'vue-cli-service serve'
 module.exports = {
+  // gh-pages sets mode = production to use subdir in url
+  publicPath: process.env.NODE_ENV === 'production' ? '/tabularasa/' : '',
   configureWebpack: {
     // Disable performance/asset size warnings due to using non-minified etc modules
     performance: { hints: false },
