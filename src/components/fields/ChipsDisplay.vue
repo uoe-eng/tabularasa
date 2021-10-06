f726b7c73c7cf65fb0ffeb4594c4ba1b8cfd20ac
Matthew Richardson <M.Richardson@ed.ac.uk>
2021-10-06 14:10:47 +0100

    Re-run fieldBaseIterable() when props change.

--- src/components/fields/ChipsDisplay.vue
+++ src/components/fields/ChipsDisplay.vue
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue'
 import Chip from 'primevue/chip'
 import fieldBase from '../fieldBase.js'
 let { useProps, fieldBaseIterable } = fieldBase()
@@ -23,7 +24,18 @@ export default {
   components: { Chip },
   props: useProps,
   setup(useProps) {
-    let { data, fieldName } = fieldBaseIterable(useProps)
+    let data = ref({})
+    let fieldName = ref('')
+    watch(
+      useProps,
+      (newProps) => {
+        let result = fieldBaseIterable(newProps)
+        data.value = result.data
+        fieldName.value = result.fieldName
+      },
+      { immediate: true }
+    )
+
     return { data, fieldName }
   },
 }
