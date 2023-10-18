db0e17e71186e1f2ed2ad8cf2e62001f41de8de8
Matthew Richardson <M.Richardson@ed.ac.uk>
2023-10-18 13:08:33 +0100

    Better handling of simple objects.

--- src/components/fields/TableDisplay.vue
+++ src/components/fields/TableDisplay.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="grid">
     <div class="col-4">
-      <label :for="'input' + field.name">{{ label }}</label>
+      <label :for="'input' + data.name">{{ label }}</label>
     </div>
     <div class="col-8">
-      <DataTable :value="field.data" :striped-rows="true" :selection-mode="selectionMode" @row-select="onRowSelect">
+      <DataTable :value="data.data" :striped-rows="true" :selection-mode="selectionMode" @row-select="onRowSelect">
         <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.label"></Column>
       </DataTable>
     </div>
@@ -16,25 +16,30 @@ import { computed, toRef } from 'vue'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
 import { trBus } from '@/index'
+import { TYPERE } from '@/helpers'
 import fieldBase from '../fieldBase.js'
-let { useProps, fieldBaseIterable } = fieldBase()
+let { useProps, fieldBaseIterable, fieldBaseValue } = fieldBase()
 
 export default {
   components: { Column, DataTable },
   props: useProps,
   setup(useProps) {
-    let field = computed(() => {
-      let fld = fieldBaseIterable(useProps)
-      // Convert single objects to an array for DataTable to display
-      if (!Array.isArray(fld)) {
-        fld = [fld]
-      }
-      return fld
-    })
-
+    let field = toRef(useProps, 'field')
     let item = toRef(useProps, 'item')
     let methods = toRef(useProps, 'methods')
     let columns = methods.value.columns()
+
+    let data = computed(() => {
+      let fld
+      if (TYPERE.test(field.value)) {
+        // Field is a nested iterable
+        fld = fieldBaseIterable(useProps)
+      } else {
+        // Field value should be an object - restructure for DataTable props
+        fld = { data: [fieldBaseValue(useProps)], name: field.value }
+      }
+      return fld
+    })
 
     const selectionMode = computed(() => {
       // Enable row selection if config schema is defined
@@ -51,7 +56,7 @@ export default {
       })
     }
 
-    return { columns, field, item, onRowSelect, selectionMode }
+    return { columns, data, item, onRowSelect, selectionMode }
   },
 }
 </script>
